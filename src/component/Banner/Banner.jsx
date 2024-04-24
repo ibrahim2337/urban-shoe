c65b2987a2476925c11d039b917e1180d7c04917
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2024-04-24 23:59:06 +0600

    banner photo ready

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -5,11 +5,15 @@ const Banner = () => {
   return (
     <div className="image1 flex items-center justify-center py-20">
       <div className="text-center">
-        <h3 className="text-white font-bold text-5xl pb-2">Academy of sport</h3>
-        <h1 className="text-white font-extrabold text-6xl pb-2">
+        <h3 className=" text-white font-bold text-5xl pb-5">
+          Academy of sport
+        </h3>
+
+        <h1 className=" text-white font-extrabold text-6xl pb-5">
           SPORTS CENTER URBAN SHOE
         </h1>
-        <p className="text-white font-bold text-2xl pb-5">
+
+        <p className="text-white font-bold text-2xl pb-10">
           Lorem Ipsum is simply dummy text of the printing
         </p>
         <button className="text-white py-2 px-5 text-center border border-white ">
