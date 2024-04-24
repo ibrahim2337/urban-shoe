2cda8c51d3e65fd0c64cef0c672b264a47595973
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2024-04-25 00:52:09 +0600

    product Card update

--- src/component/LatestProduct/LatestProduct.jsx
+++ src/component/LatestProduct/LatestProduct.jsx
@@ -8,10 +8,76 @@ const LatestProduct = () => {
           Latest product
         </h1>
       </div>
-      <div className="grid lg:grid-cols-4 lg:px-20 px-5">
+      <div className="grid lg:grid-cols-4 lg:px-20 gap-5 px-5">
         <div>
           <div className=" bg-slate-100">
-            <p className="absolute  bg-cyan-400 text-white ms-[250px] lg:ms-[197px]  px-8 py-2 text-md">
+            <p className="absolute  bg-cyan-400 text-white ms-[250px] lg:ms-[184px]  px-8 py-2 text-md">
+              NEW
+            </p>
+            <img src={product1} alt="" />
+          </div>
+          <h2 className="font-medium py-2 px-2 text-sm text-slate-500">
+            Summer Friendly Comfortable Sneakers For Men – US-65
+          </h2>
+          <div className="flex justify-between items-center px-2">
+            <div className="flex gap-2 font-medium py-2  text-xl text-slate-500">
+              <h3 className="text">$330</h3>
+              <h3 className="text-cyan-400">$250</h3>
+            </div>
+            <div className="flex gap-2 items-center font-medium text-xl">
+              <h1 className="text-slate-500">Rating :</h1>
+              <h3 className=" text-4xl mt-3 text-yellow-400">*****</h3>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className=" bg-slate-100">
+            <p className="absolute  bg-cyan-400 text-white ms-[250px] lg:ms-[184px]  px-8 py-2 text-md">
+              NEW
+            </p>
+            <img src={product1} alt="" />
+          </div>
+          <h2 className="font-medium py-2 px-2 text-sm text-slate-500">
+            Summer Friendly Comfortable Sneakers For Men – US-65
+          </h2>
+          <div className="flex justify-between items-center px-2">
+            <div className="flex gap-2 font-medium py-2  text-xl text-slate-500">
+              <h3 className="text">$330</h3>
+              <h3 className="text-cyan-400">$250</h3>
+            </div>
+            <div className="flex gap-2 items-center font-medium text-xl">
+              <h1 className="text-slate-500">Rating :</h1>
+              <h3 className=" text-4xl mt-3 text-yellow-400">*****</h3>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className=" bg-slate-100">
+            <p className="absolute  bg-cyan-400 text-white ms-[250px] lg:ms-[184px]  px-8 py-2 text-md">
+              NEW
+            </p>
+            <img src={product1} alt="" />
+          </div>
+          <h2 className="font-medium py-2 px-2 text-sm text-slate-500">
+            Summer Friendly Comfortable Sneakers For Men – US-65
+          </h2>
+          <div className="flex justify-between items-center px-2">
+            <div className="flex gap-2 font-medium py-2  text-xl text-slate-500">
+              <h3 className="text">$330</h3>
+              <h3 className="text-cyan-400">$250</h3>
+            </div>
+            <div className="flex gap-2 items-center font-medium text-xl">
+              <h1 className="text-slate-500">Rating :</h1>
+              <h3 className=" text-4xl mt-3 text-yellow-400">*****</h3>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className=" bg-slate-100">
+            <p className="absolute  bg-cyan-400 text-white ms-[250px] lg:ms-[184px]  px-8 py-2 text-md">
               NEW
             </p>
             <img src={product1} alt="" />
