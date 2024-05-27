0a9c13d295ffed9b9d130e966094c4bdf36668ca
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2024-05-27 18:29:16 +0600

    new product section ready

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Banner from "./component/Banner/Banner";
 import LatestProduct from "./component/LatestProduct/LatestProduct";
 // import MainNavbar from "./component/Navbar/MainNavbar";
 import Navbar from "./component/Navbar/Navbar";
+import NewProducts from "./component/NewProducts/NewProducts";
 import SellOff from "./component/SellOff/SellOff";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Banner />
       <LatestProduct />
       <SellOff />
+      <NewProducts />
     </div>
   );
 }
--- src/component/NewProducts/NewProducts.jsx
+++ src/component/NewProducts/NewProducts.jsx
@@ -4,12 +4,12 @@ import product3 from "../../assets/product/3.png";
 import { TiStarFullOutline } from "react-icons/ti";
 import { TiStarHalfOutline } from "react-icons/ti";
 
-const SellOff = () => {
+const NewProducts = () => {
   return (
     <div className="py-10">
       <div>
         <h1 className="uppercase text-center font-bold text-3xl">
-          / sell off .
+          / new products .
         </h1>
       </div>
       <div className="grid lg:grid-cols-4 gap-5 lg:px-28 px-5 mt-10">
@@ -112,109 +112,9 @@ const SellOff = () => {
             </div>
           </div>
         </div>
-
-        <div className=" bg-slate-100">
-          <div className="border">
-            <p className="absolute justify-center bg-cyan-400 text-white  px-[20px] py-[3px] font-bold text-md">
-              NEW
-            </p>
-            <img src={product2} alt="" />
-          </div>
-          <h2 className="font-medium py-2 px-2 text-sm text-slate-500 text-justify">
-            Summer Friendly Comfortable Sneakers For Men – US-65
-          </h2>
-          <div className="flex justify-between items-center gap-2 px-1">
-            <div className="flex justify-between items-center gap-1 font-medium py-2  text-xl text-slate-500">
-              <h3 className="font-bold">$230</h3>
-              <h3 className="text-red-600 line-through text-sm">$350</h3>
-            </div>
-            <div className="flex">
-              <TiStarFullOutline />
-              <TiStarFullOutline />
-              <TiStarFullOutline />
-              <TiStarFullOutline />
-              <TiStarHalfOutline />
-            </div>
-          </div>
-        </div>
-
-        <div className=" bg-slate-100">
-          <div className="border">
-            <p className="absolute justify-center bg-cyan-400 text-white  px-[20px] py-[3px] font-bold text-md">
-              NEW
-            </p>
-            <img src={product3} alt="" />
-          </div>
-          <h2 className="font-medium py-2 px-2 text-sm text-slate-500 text-justify">
-            Summer Friendly Comfortable Sneakers For Men – US-65
-          </h2>
-          <div className="flex justify-between items-center gap-2 px-1">
-            <div className="flex justify-between items-center gap-1 font-medium py-2  text-xl text-slate-500">
-              <h3 className="font-bold">$230</h3>
-              <h3 className="text-red-600 line-through text-sm">$350</h3>
-            </div>
-            <div className="flex">
-              <TiStarFullOutline />
-              <TiStarFullOutline />
-              <TiStarFullOutline />
-              <TiStarFullOutline />
-              <TiStarHalfOutline />
-            </div>
-          </div>
-        </div>
-
-        <div className=" bg-slate-100">
-          <div className="border">
-            <p className="absolute justify-center bg-cyan-400 text-white  px-[20px] py-[3px] font-bold text-md">
-              NEW
-            </p>
-            <img src={product1} alt="" />
-          </div>
-          <h2 className="font-medium py-2 px-2 text-sm text-slate-500 text-justify">
-            Summer Friendly Comfortable Sneakers For Men – US-65
-          </h2>
-          <div className="flex justify-between items-center gap-2 px-1">
-            <div className="flex justify-between items-center gap-1 font-medium py-2  text-xl text-slate-500">
-              <h3 className="font-bold">$230</h3>
-              <h3 className="text-red-600 line-through text-sm">$350</h3>
-            </div>
-            <div className="flex">
-              <TiStarFullOutline />
-              <TiStarFullOutline />
-              <TiStarFullOutline />
-              <TiStarFullOutline />
-              <TiStarHalfOutline />
-            </div>
-          </div>
-        </div>
-
-        <div className=" bg-slate-100">
-          <div className="border">
-            <p className="absolute justify-center bg-cyan-400 text-white  px-[20px] py-[3px] font-bold text-md">
-              NEW
-            </p>
-            <img src={product2} alt="" />
-          </div>
-          <h2 className="font-medium py-2 px-2 text-sm text-slate-500 text-justify">
-            Summer Friendly Comfortable Sneakers For Men – US-65
-          </h2>
-          <div className="flex justify-between items-center gap-2 px-1">
-            <div className="flex justify-between items-center gap-1 font-medium py-2  text-xl text-slate-500">
-              <h3 className="font-bold">$230</h3>
-              <h3 className="text-red-600 line-through text-sm">$350</h3>
-            </div>
-            <div className="flex">
-              <TiStarFullOutline />
-              <TiStarFullOutline />
-              <TiStarFullOutline />
-              <TiStarFullOutline />
-              <TiStarHalfOutline />
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
 };
 
-export default SellOff;
+export default NewProducts;
